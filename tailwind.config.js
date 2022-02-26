f7c5886be3765ccdd7cb3575a7ee373984d25196
Kyle Roberts <42209790+pTownKyle@users.noreply.github.com>
2022-02-26 04:17:29 -0500

    Fixed misspelled movie, Contact form wired, Movie list improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {},
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
